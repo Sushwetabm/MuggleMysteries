aa26ce5e58e70ae594c0d121162ae42279249c22
Sushwetabm <114595484+Sushwetabm@users.noreply.github.com>
2023-11-21 22:39:03 +0530

    minor changes

--- MM_2.cpp
+++ MM_2.cpp
@@ -12,7 +12,7 @@ public:
     string author;
     string genre;
     int pageCount;
-    Book(){}
+    Book() {}
     Book(string title, string author, string genre, int pageCount)
     {
         this->title = title;
@@ -21,8 +21,9 @@ public:
         this->pageCount = pageCount;
     }
 };
-//Database class
-class Database{
+
+// Database class
+class Database {
 public:
     unordered_map<string, Book> books;
     void addBook(Book newBook);
@@ -33,12 +34,14 @@ void Database::addBook(Book newBook)
 {
     books[newBook.title] = newBook;
 }
-Book Database::getBook()
+
+Book Database::getBook(string title)
 {
     return books[title];
 }
-//Node class
-class listNode{
+
+// Node class
+class listNode {
 public:
     vector<Book> readingList;
     listNode *left;
@@ -46,65 +49,162 @@ public:
 
     listNode(vector<Book> readingList)
     {
-      this->readingList = readingList;
-      left = NULL;
-      right = NULL;
+        this->readingList = readingList;
+        left = NULL;
+        right = NULL;
     }
 };
-class ReadingList{
+
+// Function prototypes
+listNode *insertList(listNode *root, vector<Book> newList);
+void inOrderTraversal(listNode *root);
+listNode *removeBookHelper(listNode *root, string title);
+listNode *reorderBooksHelper(listNode *root, vector<string> newOrder);
+
+// Reading List class
+class ReadingList {
 public:
     listNode *root;
 
-    ReadingList(){}
     ReadingList()
     {
         root = NULL;
     }
+
     void addList(vector<Book> readingList)
     {
-        root = insertList(root,readingList);
+        root = insertList(root, readingList);
     }
+
     void displayLists()
     {
         inOrderTraversal(root);
     }
+
+    void removeBook(string title);
+    void reorderBooks(vector<string> newOrder);
+};
+
+listNode *insertList(listNode *root, vector<Book> newList)
+{
+    if (root == NULL)
+    {
+        return new listNode(newList);
+    }
+
+    // Implement logic to insert based on a specific condition (e.g., alphabetical order)
+    // You may use the book titles for comparison
+    // For simplicity, the new list is added to the right of the current node
+    root->right = insertList(root->right, newList);
+
+    return root;
 }
 
-};
-listNode* insertList(listNode* root, vector<Book> newList)
+void inOrderTraversal(listNode *root)
 {
+    if (root == NULL)
+        return;
 
+    // Display the contents of the current node
+    for (const auto &book : root->readingList)
+    {
+        cout << "Title: " << book.title << ", Author: " << book.author << ", Genre: " << book.genre << ", Page Count: " << book.pageCount << endl;
+    }
+
+    // Traverse the left and right subtrees
+    inOrderTraversal(root->left);
+    inOrderTraversal(root->right);
 }
 
-void inOrderTraversal(listNode* root)
+void ReadingList::removeBook(string title)
 {
-
+    root = removeBookHelper(root, title);
 }
-/*
-// Reading List Node
-class ReadingListNode {
-    string listName;
-    vector<Book> books;
-};
 
-// Folder Node
-struct FolderTreeNode {
-    string folderName;
-    vector<string> readingLists; // Stores reading lists and sub-folders
-};
-
-// Book Database (Map)
-unordered_map<string, Book> bookDatabase;
-
-// Reading Lists (Graph)
-unordered_map<string, ReadingListNode> readingListGraph;
-unordered_map<string, vector<string>> adjacencyList;
-
-// Reading List Organization (Tree)
-FolderTreeNode rootFolder; // The root of the folder tree
-
-class Tree
+listNode *removeBookHelper(listNode *root, string title)
 {
+    if (root == NULL)
+        return root;
 
-};
-*/
+    // Find the book to be removed and remove it from the reading list
+    root->readingList.erase(remove_if(root->readingList.begin(), root->readingList.end(),
+                                      [title](const Book &book) { return book.title == title; }),
+                             root->readingList.end());
+
+    // Recur for the left and right subtrees
+    root->left = removeBookHelper(root->left, title);
+    root->right = removeBookHelper(root->right, title);
+
+    return root;
+}
+
+void ReadingList::reorderBooks(vector<string> newOrder)
+{
+    root = reorderBooksHelper(root, newOrder);
+}
+
+listNode *reorderBooksHelper(listNode *root, vector<string> newOrder)
+{
+    if (root == NULL)
+        return root;
+
+    vector<Book> reorderedList;
+
+    // Reorder the books based on the new order
+    for (const auto &title : newOrder)
+    {
+        auto it = find_if(root->readingList.begin(), root->readingList.end(),
+                          [title](const Book &book) { return book.title == title; });
+
+        if (it != root->readingList.end())
+        {
+            reorderedList.push_back(*it);
+        }
+    }
+
+    // Replace the existing reading list with the reordered one
+    root->readingList = reorderedList;
+
+    // Recur for the left and right subtrees
+    root->left = reorderBooksHelper(root->left, newOrder);
+    root->right = reorderBooksHelper(root->right, newOrder);
+
+    return root;
+}
+
+int main()
+{
+    // Initialize your book database and reading list
+    Database bookDB;
+    ReadingList readingLists;
+
+    // Add books to the database
+    Book book1("The Hobbit", "J.R.R. Tolkien", "Fantasy", 300);
+    Book book2("To Kill a Mockingbird", "Harper Lee", "Fiction", 350);
+    Book book3("1984", "George Orwell", "Dystopian", 400);
+
+    bookDB.addBook(book1);
+    bookDB.addBook(book2);
+    bookDB.addBook(book3);
+
+    // Create a reading list
+    vector<Book> myReadingList = {book1, book2};
+    readingLists.addList(myReadingList);
+
+    // Display reading lists
+    cout << "Initial Reading Lists:" << endl;
+    readingLists.displayLists();
+
+    // Remove a book from the reading list
+    readingLists.removeBook("To Kill a Mockingbird");
+    cout << "\nReading List after removing a book:" << endl;
+    readingLists.displayLists();
+
+    // Reorder books in the reading list
+    vector<string> newOrder = {"1984", "The Hobbit"};
+    readingLists.reorderBooks(newOrder);
+    cout << "\nReading List after reordering books:" << endl;
+    readingLists.displayLists();
+
+    return 0;
+}
